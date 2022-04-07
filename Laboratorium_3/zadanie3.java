f60c31121f5e6c90a4a507d3f4f25112440676c0
s24808 <93827195+s24808@users.noreply.github.com>
2022-04-07 13:25:05 +0200

    Create zadanie3.java

---
+++ Laboratorium_3/zadanie3.java
@@ -0,0 +1,110 @@
+class Author{
+    private String name,email;
+    private char gender;
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public char getGender() {return gender;}
+
+    public void setGender(char gender) {this.gender = gender;}
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Author[" + "name=" + getName() +
+                ",email=" + getEmail() +
+                ",gender=" + getGender() +
+                "]";
+    }
+}
+
+class Book {
+    private String name;
+    private Author[] author;
+    private double price;
+    private int qty = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author[] getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author[]) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public Book(String name, double price, Author[] author) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String name, double price, Author[] author, int qty) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public Author[] getAuthors() {
+        return author;
+    }
+
+    public String getAuthorNames() {
+        String text = "";
+        int array = getAuthors().length;
+
+        for(int i=0; i<array; i++) {
+            if(i<array-1){
+                text += author[i] + ", ";
+            }else{
+                text += author[i];
+            }
+        }
+        return text;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book[" + "name=" + getName() +
+                ",price=" + getPrice() +
+                ",authors=[" + getAuthorNames() +
+                "],qty=" + getQty() +
+                ']';
+    }
+}
