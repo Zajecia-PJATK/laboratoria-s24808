4e2f42ff0cf1d31dba9c3479e1c20f7ba4332167
s24808 <93827195+s24808@users.noreply.github.com>
2022-06-24 13:16:15 +0200

    Create Zadanie 1.java

---
+++ Laboratorium_7/Zadanie 1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+        if (!file.exists()) {
+            System.out.println("Plik nie istnieje");
+        } else {
+            System.out.println("Plik istnieje");
+        }
+    }
+}
