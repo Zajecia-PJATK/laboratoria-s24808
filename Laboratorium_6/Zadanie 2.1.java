8b1e3ba9f9766e8c8e1ecf96998876b3ad09cb5d
s24808 <93827195+s24808@users.noreply.github.com>
2022-06-24 12:07:32 +0200

    Create Zadanie 2.1.java

---
+++ Laboratorium_6/Zadanie 2.1.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.*;
+
+class Hamster {
+    private String imie;
+    private int hamsterNumber;
+    Hamster(int i) { hamsterNumber = i; }
+    public String hamsterNumber() {
+        return "This is Hamster #" + hamsterNumber;
+    }
+    public int getHamsterNumber() {
+        return hamsterNumber;
+    }
+    public String getHamsterName() {
+        return imie;
+    }
+    public void setHamsterName(String name) {
+        imie = name;
+    }
+}
+
+class Printer {
+    static void printAll(List <Hamster> hl) {
+        for(Hamster x : hl){
+            System.out.println(x.hamsterNumber());
+        }
+    }
+}
+
+public class HamsterMaze {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        List<Hamster> v = new ArrayList<Hamster>();
+        Map<String, Hamster> map = new HashMap<>();
+        
+        for(int i = 0; i < 3; i++)
+            v.add(new Hamster(i));
+        
+        for(int i = 0; i < 3; i++) {
+            String imie;
+            int id = 0;
+            Hamster hamster;
+
+            try {
+                id = s1.nextInt();
+                if (id < 0) {
+                    throw new Exception("identyfikator ujemny");
+                }
+                imie = s1.nextLine();
+            } catch (Exception e) {
+                imie = s1.nextLine();
+            }
+            hamster = new Hamster(id);
+            hamster.setHamsterName(imie);
+            map.put(hamster.getHamsterName(), hamster);
+        }
+
+        Printer.printAll(v);
+        System.out.println(map.keySet());
+    }
+}
+
