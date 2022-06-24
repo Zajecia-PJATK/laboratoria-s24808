903d5d83e4331c9cd0fd6f1b86b9c083ba4cbc51
s24808 <93827195+s24808@users.noreply.github.com>
2022-06-24 12:42:42 +0200

    Create Zadanie 2,3.java

---
+++ Laboratorium_6/Zadanie 2,3.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Hamster {
+    private String imie;
+    private int hamsterNumber;
+    Hamster(int i) { hamsterNumber = i; }
+    public String hamsterNumber() {
+        return "This is Hamster #" + hamsterNumber;
+    }
+    public int getHamsterNumber() {
+        return hamsterNumber;
+    }
+    public String getHamsterName() {
+        return imie;
+    }
+    public void setHamsterName(String name) {
+        imie = name;
+    }
+}
+
+class Printer {
+    static void printAll(List <Hamster> hl) {
+        ListIterator<Hamster> listIterator = hl.listIterator();
+        while (listIterator.hasNext()){
+            System.out.println(listIterator.next().hamsterNumber());
+        }
+    }
+}
+
+ class HamsterMaze {
+    ArrayList<Hamster> removeHamster(int number, ArrayList<Hamster> delete){
+        ListIterator<Hamster> remove = delete.listIterator();
+        for(int i=0; i < number; i++){
+            remove.remove();
+        }
+        return delete;
+    }
+
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        ArrayList<Hamster> v = new ArrayList<Hamster>();
+        Map<String, Hamster> map = new HashMap<>();
+        ListIterator<Hamster> remove = v.listIterator();
+        
+        for(int i = 0; i < 3; i++)
+            v.add(new Hamster(i));
+        
+        for(int i = 0; i < 3; i++) {
+            String imie = "";
+            int id = 0;
+            Hamster hamster;
+
+            try {
+                id = s1.nextInt();
+                if (id < 0) throw new Exception("identyfikator ujemny");
+                imie = s1.nextLine();
+            } catch (Exception e) {
+                if (s1.hasNextInt()) {
+                    id = s1.nextInt();
+                }
+                if (s1.hasNextLine()) {
+                    imie = s1.nextLine();
+                }
+            }
+            hamster = new Hamster(id);
+            hamster.setHamsterName(imie);
+            map.put(hamster.getHamsterName(), hamster);
+        }
+
+        Printer.printAll(v);
+        System.out.println(map.keySet());
+    }
+}
